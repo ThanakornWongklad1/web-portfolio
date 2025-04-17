5ca44a19b7d72e652ecf3b0f077ab2a734eae53e
Thanakorn Wongklad <wongkladt@gmail.com>
2025-04-18 05:09:20 +0700

    section 1 and layout for section2

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,8 @@
 <template>
-  <p class="text-3xl w-full mx-auto">Hello World</p>
-  <NuxtMarquee
-    class="text-3xl w-full mx-auto"
-    :text="'Hello World'"
-    :duration="10000"
-    :delay="0"
-    :direction="'left'"
-  >
-    <span class="text-3xl w-full mx-auto">Hello World</span>
-    <span class="text-3xl w-full mx-auto">Hello World</span>
-  </NuxtMarquee>
+  <div class="w-full">
+    <!-- section1 -->
+    <Section1Component />
+    <!-- section2 -->
+    <Section2Component />
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,39 @@
+import { modal } from "#build/ui";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/google-fonts","nuxt-marquee"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/google-fonts", "nuxt-marquee"],
 
   css: ["~/assets/css/main.css"],
 
-  future: {
-    compatibilityVersion: 4,
-  },
-
   googleFonts: {
-    display: 'swap',
+    display: "swap",
     families: {
       Poppins: {
         wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
         ital: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      }
+      },
+    },
+  },
+
+  app: {
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "My personal web portfolio by Thanakorn Wongklad",
+        },
+        { name: "author", content: "Thanakorn Wongklad" },
+      ],
     },
   },
 
   compatibilityDate: "2024-11-27",
+
+  future: {
+    compatibilityVersion: 4,
+  },
 });
